80d677ab4f851512adeb7bfee5158f0e30e34f54
Vasilii Alexeev <1a2v3a@gmail.com>
2022-12-18 23:16:16 +0500

    first commit

---
+++ lua/alex/init.lua
@@ -0,0 +1,13 @@
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+require("alex.packer")
+require("alex.remap")
+require("alex.config.lualine")
+require("alex.config.thems")
+require("alex.config.mason")
+require("alex.config.lsp")
+require("alex.config.lspkind")
+
+
+
